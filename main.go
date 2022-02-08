23fa1d96d16d055462d1a3bd40b9f7856c079ac4
Saintmalik <msalawu5@gmail.com>
2022-02-08 13:06:54 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    // "fmt"
+    // "strings"
+    "html/template"
+    "net/http"
+)
+
+var tpl *template.Template
+
+func init(){
+    tpl = template.Must(template.ParseGlob("templates/*.html",))
+}
+
+func wame(w http.ResponseWriter, r *http.Request){
+    tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func main() {
+    http.HandleFunc("/", wame)
+    http.HandleFunc("/walink", walink)
+    http.ListenAndServe(":3001", nil)
+}
+
+func walink(w http.ResponseWriter, r *http.Request){
+    if r.Method != "POST" {
+        http.Redirect(w, r, "/", http.StatusSeeOther )
+        return
+}
+    countrycode := r.FormValue("cid")
+    numbers :=  r.FormValue("numbs")
+
+    d := struct {
+        Country string
+        Num string
+    }{
+        Country:  countrycode,
+        Num: numbers[1:],
+    }
+
+tpl.ExecuteTemplate(w, "walink.html", d)
+}
